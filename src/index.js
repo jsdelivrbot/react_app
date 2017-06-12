5d05e7338393ffab99593fe188f06ce0e1b39ad3
“katyvonkaty” <kj0203@gmail.com>
2017-06-11 22:18:59 -0400

    webpack complies

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Create a new component. This component should produce some HTML
+
+const App = function(){
+
+  return <div> Hi! </div>;
+}
+
+// Take this components generated HTML and put it in the dom
